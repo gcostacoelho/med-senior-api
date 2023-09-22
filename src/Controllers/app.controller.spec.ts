983b7deb11982b89124237da8e09b9309585764c
gustavo <gcostacoelho2003@gmail.com>
2023-09-22 13:17:42 -0300

    openapi-config

--- src/Controllers/app.controller.spec.ts
+++ src/Controllers/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './Controllers/app.controller';
-import { AppService } from './Services/app.service';
+import { AppController } from './app.controller';
+import { AppService } from '../Services/app.service';
 
 describe('AppController', () => {
     let appController: AppController;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const configSwagger = new DocumentBuilder()
+        .setTitle('MedSenior - API')
+        .setDescription('MedSenior')
+        .setVersion('1.0.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, configSwagger);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
