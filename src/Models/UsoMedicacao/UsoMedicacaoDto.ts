ea390ea635835f5a6e6deca88ba71417f2551fd6
Rezende-Fabio <rezendefabio337@gmail.com>
2023-10-07 11:06:39 -0300

    adjusting swagger response

--- src/Models/UsoMedicacao/UsoMedicacaoDto.ts
+++ src/Models/UsoMedicacao/UsoMedicacaoDto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { MedicacaoDto } from "../Medicacao/MedicacaoDto";
 
 export class UsoMedicacaoDto {
     @ApiProperty()
@@ -18,4 +19,27 @@ export class UsoMedicacaoDto {
 
     @ApiProperty()
     medId: string
+}
+// Response
+export class UsoMedicacaoResponseDto {
+    @ApiProperty()
+    dosagem: number;
+  
+    @ApiProperty()
+    intervalo: number;
+  
+    @ApiProperty()
+    horaInicial: Date;
+  
+    @ApiProperty()
+    dataFinal: Date;
+  
+    @ApiProperty()
+    idosoId: string;
+  
+    @ApiProperty()
+    medId: string;
+  
+    @ApiProperty()
+    medicacao: MedicacaoDto; // Apenas na resposta
 }
